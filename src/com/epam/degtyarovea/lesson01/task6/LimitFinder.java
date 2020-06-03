50944931ba2c8e47479643b9624622aeac498622
degtyaryov <Degtyarov96@mail.ru>
2020-06-03 16:00:47 +0300

    Lesson 1: Refactoring task6.

--- src/com/epam/degtyarovea/lesson01/task6/LimitFinder.java
+++ src/com/epam/degtyarovea/lesson01/task6/LimitFinder.java
@@ -1,19 +1,13 @@
 package com.epam.degtyarovea.lesson01.task6;
 
-import java.util.Scanner;
-
 public class LimitFinder {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input 'a1' and 'step: ");
-        int a1 = scanner.nextInt();
-        int step = scanner.nextInt();
+    public long findLimitForInt(int a1, int step) {
+        return find(a1, step, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
 
-        LimitFinder finder = new LimitFinder();
-        long intN = finder.find(a1, step, Integer.MIN_VALUE, Integer.MAX_VALUE);
-        long longN = finder.find(a1, step, Long.MIN_VALUE, Long.MAX_VALUE);
-        System.out.printf("N for integer = %d, for long = %d.", intN, longN);
+    public long findLimitForLong(int a1, int step) {
+        return find(a1, step, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
     private long find(int a1, int step, long minRange, long maxRange) {
