530a66ccd4c83c3c427a63f09b1d50f45180dd92
degtyaryov <Degtyarov96@mail.ru>
2020-06-06 10:56:07 +0300

    Lesson 1: Simplify task4.

---
+++ src/by/epamtc/degtyarovea/lesson01/task4/Function.java
@@ -0,0 +1,18 @@
+package by.epamtc.degtyarovea.lesson01.task4;
+
+import static java.lang.Math.pow;
+
+public class Function {
+
+    public static void main(String[] args) {
+        double x = 12.35;
+        double functionValue;
+
+        functionValue = getFunctionValue(x);
+        System.out.printf("Function value is %.2f\n", functionValue);
+    }
+
+    public static double getFunctionValue(double x) {
+        return (x > 13) ? -3 / (x + 1) : -pow(x, 3) + 9;
+    }
+}
